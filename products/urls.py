a277eea7913a934ecb4e512f0803ecb573ba59ac
ehsan <ehsan84rz@gmail.com>
2023-08-25 16:35:50 +0330

    Implement ProductCreate view/url/page & add LoginRequiredMixin

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, ProductUpdateView, ProductDeleteView
+from .views import ProductListView, ProductDetailView, ProductUpdateView, ProductDeleteView, ProductCreateView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('<int:pk>/edit', ProductUpdateView.as_view(), name='product_update'),
     path('<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .models import Product
@@ -17,15 +20,24 @@ class ProductDetailView(DetailView):
     template_name = 'products/product_detail.html'
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     context_object_name = 'product'
     form_class = NewProductForm
-    template_name = 'products/product_create.html'
+    template_name = 'products/product_update.html'
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     context_object_name = 'product'
     success_url = reverse_lazy('products:product_list')
     template_name = 'products/product_delete.html'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    form_class = NewProductForm
+    template_name = 'products/product_create.html'
+
+    def form_valid(self, form):
+        form.instance.seller = self.request.user
+        return super().form_valid(form)
