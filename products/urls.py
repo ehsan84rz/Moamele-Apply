fd19087649d047cc42c5b51a6f157a1dea0d16e8
ehsan <ehsan84rz@gmail.com>
2023-08-25 15:54:47 +0330

    Implement ProductUpdate view/url/page

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductUpdateView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('<int:pk>/edit', ProductUpdateView.as_view(), name='product_update'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 
 from .models import Product
+from .forms import NewProductForm
 
 
 class ProductListView(ListView):
@@ -13,3 +14,10 @@ class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'products/product_detail.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    context_object_name = 'product'
+    form_class = NewProductForm
+    template_name = 'products/product_create.html'
